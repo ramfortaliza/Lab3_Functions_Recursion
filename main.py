e5a8f1ae07ee230439b39fbd4dd04ad996bf6a53
Ram Cedric Fortaliza <ramfortaliza@gmail.com>
2026-03-02 14:08:56 +0800

    Completed Activity 3: Functions and Modules by Fortaliza

---
+++ main.py
@@ -0,0 +1,31 @@
+#main.py
+
+import grades
+
+#Student ID Config
+LAST_NAME = "Fortaliza"
+STUDENT_ID = "TUPM-25-0186"
+
+SEED_DIGIT = int(STUDENT_ID[-1])
+ID_SUM = sum(int(d) for d in STUDENT_ID if d.isdigit())
+NAME_LENGTH = len(LAST_NAME)
+
+#Student Unique Scores
+scores = [
+    SEED_DIGIT * 10,
+    ID_SUM % 100,
+    NAME_LENGTH * 7
+]
+
+average = grades.compute_average(scores)
+grade = grades.assign_grade(average)
+remark = grades.generate_remark(grade)
+
+print("=" * 40)
+print(f"Student: {LAST_NAME}")
+print(f"Student ID: {STUDENT_ID}")
+print(f"Generated Scores: {scores}")
+print(f"Average: {round(average,2)}")
+print(f"Grade: {grade}")
+print(f"Remark: {remark}")
+print("=" * 40)
